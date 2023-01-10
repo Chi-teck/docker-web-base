76955a02bc238d592cb55a30cc76372d22d8524a
Chi-teck <Chi-teck@users.noreply.github.com>
2023-01-10 18:23:59 +0500

    Fix user directory in dev-tools playbook

--- playbooks/dev-tools/post-install.sh
+++ playbooks/dev-tools/post-install.sh
@@ -5,6 +5,6 @@ set -Eeuo pipefail
 if [[ -n ${PLAYBOOK_USER:-} ]]; then
     mc_ini=/home/$USER_NAME/.config/mc/ini
     if [[ -f $mc_ini ]]; then
-       sed -i 's/skin=default/skin=julia256/g' /home/broker/.config/mc/ini
+       sed -i 's/skin=default/skin=julia256/g' /home/$USER_NAME/.config/mc/ini
     fi
 fi
