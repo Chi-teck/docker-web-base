0ebf89aefeebad189df0a79108269e5c14a551b6
Chi-teck <Chi-teck@users.noreply.github.com>
2023-01-22 22:44:00 +0500

    Add user to 'www-data' group

--- playbooks/user/post-install.sh
+++ playbooks/user/post-install.sh
@@ -4,3 +4,7 @@ home_dir=/home/$USER_NAME;
 echo -e "\ncd ${WEB_ROOT:-$home_dir}" >> $home_dir/.bashrc
 
 chown -R $USER_NAME:$USER_NAME /home/$USER_NAME
+
+if [[ -n ${PLAYBOOK_APACHE:-} || -n ${PLAYBOOK_NGINX:-} ]]; then
+    usermod -a -G www-data $USER_NAME
+fi
