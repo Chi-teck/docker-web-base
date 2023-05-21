3a88f70bca161850a109c9acfc691fcf4574f67b
Chi-teck <Chi-teck@users.noreply.github.com>
2023-05-21 18:43:47 +0500

    Fix typo

--- playbooks/postgresql/start.sh
+++ playbooks/postgresql/start.sh
@@ -2,7 +2,7 @@
 
 set -Eeuo pipefail
 
-# Return orignal PgSQL directory if the mounted one is empty.
+# Return original postgres directory if the mounted one is empty.
 if [ ! "$(ls -A "/var/lib/postgresql/15/main")" ]; then
   cp -pRT /var/lib/postgresql_init /var/lib/postgresql/15/main
 fi
