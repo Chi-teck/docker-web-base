b28bc3dec154126a547d152cd30772a1ea113ba8
Chi-teck <Chi-teck@users.noreply.github.com>
2024-11-13 23:39:46 +0500

    Clean-up

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,8 +6,8 @@ function wb_label {
   echo -e "\n\e[30;46m $* \e[0m\n"
 }
 
-php_versions='7.3 7.4 8.0 8.1 8.2 8.3'
-php_latest_version='8.3'
+readonly php_versions='7.3 7.4 8.0 8.1 8.2 8.3'
+readonly php_latest_version='8.3'
 images_dir=$(dirname "$(readlink -f "$0")")/../images
 
 target=all
@@ -27,13 +27,11 @@ done
 
 for image in dev nginx; do
   if [[ $target = all || $target = "$image" ]]; then
-
     image_id=attr/$image
     wb_label $image_id
     docker build -t $image_id $images_dir/$image
     [[ $push = true ]] && docker push $image_id
-    [[ $remove = true ]] && docker rmi $image_id || true
-
+    [[ $remove = true ]] && (docker rmi $image_id || true)
   fi
 done
 
@@ -45,14 +43,14 @@ for image in dev-apache-php dev-php-fpm php-fpm; do
       wb_label $image_id
       docker build -t $image_id --build-arg="PHP_VERSION=$php_version" $images_dir/$image
       [[ $push = true ]] && docker push $image_id
-      [[ $remove = true ]] && docker rmi $image_id || true
+      [[ $remove = true ]] && (docker rmi $image_id || true)
 
       if [[ $php_version = "$php_latest_version" ]]; then
         image_id=attr/$image:latest
         wb_label $image_id
         docker build -t $image_id --build-arg="PHP_VERSION=$php_version" $images_dir/$image
         [[ $push = true ]] && docker push $image_id
-        [[ $remove = true ]] && docker rmi $image_id || true
+        [[ $remove = true ]] && (docker rmi $image_id || true)
       fi
 
     fi
