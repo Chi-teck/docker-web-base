f3f7060e2007a6dfdcb4ab6cc5fa285bc44bc88c
Chi-teck <Chi-teck@users.noreply.github.com>
2026-03-28 14:32:13 +0500

    Overhaul dev images: PS1, gitconfig, Composer, cert

--- images/dev-apache-php/create-cert.sh
+++ images/dev-apache-php/create-cert.sh
@@ -2,17 +2,10 @@
 
 set -Eeuo pipefail
 
-openssl req \
+openssl req -x509 -nodes \
   -newkey rsa:2048 \
-  -x509 \
-  -nodes \
-  -keyout server.pem \
-  -new \
   -keyout /etc/ssl/private/localhost.key \
   -out /etc/ssl/certs/localhost.crt \
   -subj /CN=localhost \
-  -reqexts SAN \
-  -extensions SAN \
-  -config <(cat /usr/lib/ssl/openssl.cnf <(printf '[SAN]\nsubjectAltName=DNS:localhost')) \
   -sha256 \
   -days 365
